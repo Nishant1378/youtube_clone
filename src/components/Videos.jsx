908439a3865f9dcdc9e49b80a52b7fb7fea8ddc5
Nishant Pawar <nishantpawar1378@gmail.com>
2024-03-08 21:41:34 +0530

    video details done

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Stack, Box } from '@mui/material';
+import { VideoCard, ChannelCard } from './';
 
-const Videos = () => {
+const Videos = ({ videos }) => {
+  // Check if videos is null or not an array
+  if (!Array.isArray(videos) || videos.length === 0) {
+    return <div>No videos available</div>;
+  }
+
   return (
-    <div>Videos</div>
-  )
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}> 
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>  
+  );
 }
 
-export default Videos
+export default Videos;
